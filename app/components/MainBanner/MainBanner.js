48667e047615e6bf997a64cda4c7145b7c730d21
lkai1 <kai-le@outlook.com>
2025-07-04 01:28:45 +0300

    change main banner image

--- app/components/MainBanner/MainBanner.js
+++ app/components/MainBanner/MainBanner.js
@@ -7,7 +7,7 @@ const MainBanner = () => {
 
 	return (
 		<div className={styles.main}>
-			<img src={`${basePath}/images/main_banner.jpg`} className={styles.backgroundImg} />
+			<img src={`${basePath}/images/main_banner.png`} className={styles.backgroundImg} />
 		</div>
 	)
 }
